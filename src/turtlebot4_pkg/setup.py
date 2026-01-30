21a21876e19e33a5eb181e1c60667f179af68498
dlwnsh925 <dlwnsh925@gmail.com>
2026-01-30 13:57:57 +0900

    [Fix] 로봇 도킹 문제 해결 및 goal_pose로 이동
    카메라 딜레이가 심해 인식하기도 전에 goal_pose로 이동 완료하는 문제 발생함.

--- src/turtlebot4_pkg/setup.py
+++ src/turtlebot4_pkg/setup.py
@@ -27,6 +27,7 @@ setup(
             'yolo = turtlebot4_pkg.2_4_g_yolov8_obj_det:main',
             'depth = turtlebot4_pkg.depth_checker:main',
             'move = turtlebot4_pkg.3_2_a_nav_to_pose:main',
+            'move_origin = turtlebot4_pkg.nav_to_pose_origin:main'
         ],
     },
 )
--- src/turtlebot4_pkg/turtlebot4_pkg/3_2_a_nav_to_pose.py
+++ src/turtlebot4_pkg/turtlebot4_pkg/3_2_a_nav_to_pose.py
@@ -1,77 +1,3 @@
-# import rclpy
-# from rclpy.node import Node
-# from detect_msg.msg import Rcinfo
-
-# from turtlebot4_navigation.turtlebot4_navigator import TurtleBot4Directions, TurtleBot4Navigator
-
-# class Move(Node):
-#     def __init__(self):
-#         super().__init__('move_node')
-
-#         self.subscription = self.create_subscription(
-#             Rcinfo,
-#             'detected_msg',
-#             self.detect_sub_callback,
-#             10)
-        
-#     def detect_sub_callback(self, msg):
-#         self.detected = msg.detected
-#         self.pt_map = msg.map
-#         self.distance = msg.dist 
-
-
-
-# def main():
-#     rclpy.init()
-
-#     navigator = TurtleBot4Navigator()
-
-#     # Start on dock
-#     if not navigator.getDockedStatus():
-#         navigator.info('Docking before intialising pose')
-#         navigator.dock()
-
-#     # Set initial pose
-#     initial_pose = navigator.getPoseStamped([0.0, 0.0], TurtleBot4Directions.NORTH)
-#     navigator.setInitialPose(initial_pose)
-
-#     # Wait for Nav2
-#     navigator.waitUntilNav2Active()
-
-#     # Set goal poses
-#     # goal_pose = navigator.getPoseStamped([-13.0, 9.0], TurtleBot4Directions.EAST)
-#     ### code change #####
-#     goal_pose = navigator.getPoseStamped([-1.998, 0.818], TurtleBot4Directions.EAST)
-
-# #   Position(-1.55069, 0.0668084, 0), Orientation(0, 0, -0.962154, 0.272507) = Angle: -2.5896
-
-
-#     # Undock
-#     navigator.undock()
-
-#     # Go to each goal pose
-#     navigator.startToPose(goal_pose)
-
-#     rclpy.shutdown()
-
-#     # rclpy.init()
-#     # move_node = Move()
-
-#     # try:
-#     #     while rclpy.ok() and not move_node.should_exit:
-#     #         rclpy.spin(move_node, timeout_sec=0.1)
-#     # except KeyboardInterrupt:
-#     #     pass
-#     # finally:
-#     #     move_node.destroy_node()
-#     #     rclpy.shutdown()
-
-
-# if __name__ == '__main__':
-#     main()
-
-
-
 import rclpy
 from rclpy.node import Node
 from detect_msg.msg import Rcinfo
@@ -85,10 +11,10 @@ class Move(Node):
         self.navigator = navigator
         self.detected = False
         self.pt_map = None
-        self.distance = 0.0
-        self.should_exit = False
-        self.navigation_initialized = False
-        self.previous_detected = None  # 이전 상태 추적
+        # self.distance = 0.0
+        # self.should_exit = False
+        # self.navigation_initialized = False
+        # self.previous_detected = None  # 이전 상태 추적
         
         # 초기 목표 위치 설정
         self.default_goal = [-1.998, 0.818]
@@ -102,9 +28,9 @@ class Move(Node):
         # 타이머로 navigation 초기화 및 실행
         self.timer = self.create_timer(0.5, self.navigation_callback)
 
-        self.get_logger().info(f'self.navigation_initialized:{self.navigation_initialized}')
+        # self.get_logger().info(f'self.navigation_initialized:{self.navigation_initialized}')
             
-        
+        # 인지값 받아오는 서브스크라이버 콜백함수
     def detect_sub_callback(self, msg):
         self.detected = msg.detected
         self.pt_map = msg.map  # geometry_msgs/PointStamped
@@ -119,31 +45,11 @@ class Move(Node):
         else:
             self.get_logger().info(f'Received: detected={self.detected}, dist={self.distance}')
     
+    # spin 도는 콜백함수, 목표 위치로 이동
     def navigation_callback(self):
-        self.get_logger().info(f'nav callback, {self.navigation_initialized}')
-        # 한 번만 초기화
-        if not self.navigation_initialized:
-            
-            self.get_logger().info(f'self.navigation_initialized:{self.navigation_initialized}')
-            
-            # Start on dock
-            # if not self.navigator.getDockedStatus():
-            #     self.navigator.info('Docking before initialising pose')
-            #     self.navigator.dock()
-            self.navigator.dock()
-
-            # Set initial pose
-            initial_pose = self.navigator.getPoseStamped([0.0, 0.0], TurtleBot4Directions.NORTH)
-            self.navigator.setInitialPose(initial_pose)
-
-            # Wait for Nav2
-            self.navigator.waitUntilNav2Active()
-
-            # Undock
-            self.navigator.undock()
-            
-            self.get_logger().info('Navigation initialized. Monitoring detection...')
-            self.navigation_initialized = True
+        # self.get_logger().info(f'nav callback, {self.navigation_initialized}')
+        # 네비게이터가 이미 다른 명령을 수행 중이면 건너뜀
+        if not self.navigator.isTaskComplete():
             return
         
         # detected 상태가 변경되었을 때만 새로운 목표로 이동
@@ -176,7 +82,26 @@ def main():
     rclpy.init()
     
     navigator = TurtleBot4Navigator()
+
+    # 최종에서는 주석 해제 필요
+    # # Start on dock
+    if not navigator.getDockedStatus():
+        navigator.info('Docking before initialising pose')
+        navigator.dock()
+
+    # Set initial pose
+    initial_pose = navigator.getPoseStamped([0.0, 0.0], TurtleBot4Directions.NORTH)
+    navigator.setInitialPose(initial_pose)
+
+    # Wait for Nav2
+    navigator.waitUntilNav2Active()
+
+    # # Undock
+    navigator.undock()
+    
     move_node = Move(navigator)
+
+    
 
     try:
         rclpy.spin(move_node)
